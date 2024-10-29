44360526d49f22f1fd021b954fa2821953b2fd75
Tomkoid <tomaszierl@outlook.com>
2024-10-29 22:48:39 +0100

    feat: enable cors on the backend
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -2,10 +2,13 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func StartServer() {
 	e := echo.New()
+	e.Use(middleware.CORS())
+
 	e.HideBanner = true
 
 	apiV1 := e.Group("/api/v1")
