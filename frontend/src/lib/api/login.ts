ced4da897016054ba7ab2fd22861b4236785c9e2
Tomkoid <tomaszierl@outlook.com>
2024-10-29 23:02:08 +0100

    feat: implement login on frontend
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- frontend/src/lib/api/login.ts
+++ frontend/src/lib/api/login.ts
@@ -1,65 +1,16 @@
-interface User {
-  cislo: string,
-  dochazka: string,
-  editaceJidelnicku: number,
-  email: string,
-  id: string,
-  jmeno: string,
-  konto: string,
-  limit: string,
-  mena: string,
-  nazevJidelny: string,
-  pasivni: boolean,
-  pocetJidel: boolean,
-  podrobnosti: boolean,
-  prihlaska: boolean,
-  slabeHeslo: boolean,
-  text: string,
-  verze: string,
-  vicenasobny: boolean,
-  vs: string,
-  vydej: number,
-}
-
 interface LoginResponse {
-  betatest: boolean,
-  cislo: string,
-  ignoreCert: boolean,
-  jmeno: string,
-  s5url: string,
-  sid: string,
-  uzivatel: User,
-  verze: string,
-  zustatPrihlasen: true,
+  canteen: number,
+  sid: number
 }
 // returns if logged in
 export async function login(username: string, password: string, canteen: string) {
-  let req = await fetch("https://app.strava.cz/api/login", {
+  let params = new URLSearchParams()
+
+  params.append("username", username)
+  params.append("password", password)
+  params.append("canteen", canteen)
+  let req = await fetch(`http://127.0.0.1:1323/api/v1/login?${params.toString()}`, {
     method: "POST",
-    referrer: "https://app.strava.cz/en/prihlasit-se?jidelna",
-    referrerPolicy: "strict-origin-when-cross-origin",
-    headers: {
-      "accept": "*/*",
-      "accept-language": "en-US,en;q=0.9",
-      "content-type": "text/plain;charset=UTF-8",
-      "priority": "u=1, i",
-      "sec-ch-ua": "\"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"130\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Linux\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "sec-gpc": "1"
-    },
-    mode: "no-cors",
-    body: JSON.stringify({
-      cislo: canteen,
-      jmeno: username,
-      heslo: password,
-      zustatPrihlasen: true,
-      environment: "W",
-      lang: "EN"
-    })
   })
 
   if (req.status != 200) {
@@ -68,4 +19,11 @@ export async function login(username: string, password: string, canteen: string)
 
   let data: LoginResponse = await req.json()
   console.log(data)
+
+  localStorage.setItem("sid", data.sid.toString())
+  localStorage.setItem("canteen", data.canteen.toString())
+
+  localStorage.setItem("username", username)
+  localStorage.setItem("password", password)
+  localStorage.setItem("canteen", canteen)
 }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,42 @@
 <script lang="ts">
 	import { login } from '$lib/api/login';
-	let username = $state('test');
-	let password = $state('testpass');
-	let canteen = $state('0000');
+	import { onMount } from 'svelte';
+
+	let username = $state('');
+	let password = $state('');
+	let canteen = $state('');
+	let errorMessage = $state('');
+	onMount(() => {
+		if (localStorage) {
+			localStorage.getItem('username') ? (username = localStorage.getItem('username')!) : null;
+			localStorage.getItem('password') ? (password = localStorage.getItem('password')!) : null;
+			localStorage.getItem('canteen') ? (canteen = localStorage.getItem('canteen')!) : null;
+		}
+	});
 </script>
 
 <div class="flex flex-col mt-5">
-	<input class="login-input" type="username" bind:value={username} />
-	<input class="login-input" type="password" bind:value={password} />
-	<input class="login-input" bind:value={canteen} />
+	<input
+		class="login-input"
+		type="username"
+		placeholder="Uživatelské jméno"
+		bind:value={username}
+	/>
+	<input class="login-input" type="password" placeholder="Heslo" bind:value={password} />
+	<input class="login-input" placeholder="Jídelna" bind:value={canteen} />
 	<button
 		class="bg-lime-400 border p-2"
 		onclick={() => {
-			login(username, password, canteen);
+			login(username, password, canteen).catch((e) => {
+				console.log(typeof e);
+				if (typeof e === 'string') {
+					errorMessage = e;
+				} else if (e instanceof Error) {
+					errorMessage = e.message;
+				}
+			});
 		}}>Login</button
 	>
+	<p class="text-red-600">{errorMessage}</p>
 </div>
 
