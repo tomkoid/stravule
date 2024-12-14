b2d928020c1b1d664deb250973c132da302c1ee9
Tomkoid <tomaszierl@outlook.com>
2024-12-14 18:31:58 +0100

    feat: add calendar ui
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- frontend/src/lib/components/orders/settings/index.ts
+++ frontend/src/lib/components/orders/settings/index.ts
@@ -1,8 +1,10 @@
 import Root from "./Settings.svelte"
+import Calendar from "./Calendar.svelte"
 import DayExceptions from "./DayExceptions.svelte"
 
 export {
   Root,
+  Calendar,
   DayExceptions,
 
   Root as Settings,
